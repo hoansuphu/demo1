fccd0b3104d0606caddb12adc1394ee540dd2c8f
hoannguyen <hoan.nguyen.it@gmail.com>
2016-01-03 00:12:35 +0800

    added deduct function

--- src/demo1/main/Main1.java
+++ src/demo1/main/Main1.java
@@ -5,6 +5,7 @@ public class Main1 {
 	public static void main(String[] args) {
 
 		System.out.println("Sum 02 number 1 & 2: " + Utils.sum(1, 2));
+		System.out.println("Subtract 1 from 2: " + Utils.deduct(1, 2));
 	}
 
 }
--- src/demo1/main/Utils.java
+++ src/demo1/main/Utils.java
@@ -5,4 +5,8 @@ public class Utils {
 	public static Number sum(Number n1, Number n2){
 		return n1.doubleValue() + n2.doubleValue();
 	}
+	
+	public static Number deduct(Number value, Number fromValue){
+		return fromValue.doubleValue() - value.doubleValue();
+	}
 }
