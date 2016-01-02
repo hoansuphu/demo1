30f93ce7977a69727495aa1aa45eb545f4d22a8c
hoannguyen <hoan.nguyen.it@gmail.com>
2016-01-03 00:47:17 +0800

    added Main1

--- src/demo1/main/Main1.java
+++ src/demo1/main/Main1.java
@@ -4,8 +4,10 @@ public class Main1 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Sum 02 number 1 & 2: " + Utils.sum(1, 2));
-		System.out.println("Subtract 1 from 2: " + Utils.deduct(1, 2));
+		System.out.println("hello world");
+		
+		System.out.println("Sum 02 number 1 & 2: " + Calculator.sum(1, 2));
+		System.out.println("Subtract 1 from 2: " + Calculator.deduct(1, 2));
 	}
 
 }
