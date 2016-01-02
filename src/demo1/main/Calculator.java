f297f62e280451d1b1ccdada0c9d7ef2a454d935
hoannguyen <hoan.nguyen.it@gmail.com>
2016-01-03 00:53:18 +0800

    rename calculator & add multiply function

--- src/demo1/main/Calculator.java
+++ src/demo1/main/Calculator.java
@@ -1,6 +1,6 @@
 package demo1.main;
 
-public class Utils {
+public class Calculator {
 
 	public static Number sum(Number n1, Number n2){
 		return n1.doubleValue() + n2.doubleValue();
@@ -8,5 +8,9 @@ public class Utils {
 	
 	public static Number deduct(Number value, Number fromValue){
 		return fromValue.doubleValue() - value.doubleValue();
+	}
+	
+	public static Number multiply(Number n1, Number n2){
+		return n1.doubleValue() * n2.doubleValue();
 	}
 }
